98634cab2cebe32fdbc688aeb006e74fbe57754f
SW.CHAE <doortts@gmail.com>
2012-11-11 17:48:11 +0900

    Update jquery.sortable.js
    remove "dt.setData('Text', 'dummy');" 
    It prevents to use user data transferring.

--- jquery.sortable.js
+++ jquery.sortable.js
@@ -40,7 +40,6 @@ $.fn.sortable = function(options) {
 			isHandle = false;
 			var dt = e.originalEvent.dataTransfer;
 			dt.effectAllowed = 'move';
-			dt.setData('Text', 'dummy');
 			index = (dragging = $(this)).addClass('sortable-dragging').index();
 		}).on('dragend.h5s', function() {
 			if (!dragging) {
